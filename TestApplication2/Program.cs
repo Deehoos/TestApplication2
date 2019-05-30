2c1055f5d8621c67dbaf20aa22b7ac0f9c2701b7
Dylan Hughes <dhughes238x@gmail.com>
2019-05-30 13:25:25 +0800

    Test comments

--- TestApplication2/Program.cs
+++ TestApplication2/Program.cs
@@ -15,6 +15,7 @@ namespace TestApplication2
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+            //test comment
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
